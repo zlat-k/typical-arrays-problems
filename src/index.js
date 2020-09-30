adfd5c9aec2329ac54fa0115b46f00a7bdf33247
zlat-k <64163122+zlat-k@users.noreply.github.com>
2020-09-30 22:00:36 +0300

    feat: upload index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    let tmpArrMin = array.slice()
+    tmpArrMin = tmpArrMin.sort(function (a, b) {
+        return a - b;
+    });
+    return tmpArrMin[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    let tmpArrMax = array.slice()
+    tmpArrMax = tmpArrMax.sort(function (a, b) {
+        return a - b;
+    });
+    return tmpArrMax[tmpArrMax.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length === 0) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    let tmpArrAvg = array.slice()
+    let tmp = 0;
+    for (let i = 0; i < tmpArrAvg.length; i++) {
+        tmp = tmp + tmpArrAvg[i];
+    }
+    return tmp/ tmpArrAvg.length
 }
+
